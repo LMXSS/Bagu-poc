b566b3930ce909229a74952600f651b7213b7fc4
Gabriel Lamas <glamas.2003@gmail.com>
2025-05-28 20:03:17 -0300

    firts commit

---
+++ bagu-poc/src/App.tsx
@@ -0,0 +1,13 @@
+import Banner from './components/sections/banner'
+
+function App() {
+
+  return (
+    <div className='h-screen  w-full '>
+    <Banner></Banner>
+    </div> 
+   
+  )
+}
+
+export default App
---
+++ bagu-poc/src/components/sections/banner/index.tsx
@@ -0,0 +1,13 @@
+import banner from '@/assets/banner.webp'
+
+function Banner() {
+
+  return (
+   <img style={{
+    width:'100vw',
+    height:300
+   }} src={banner}/>
+  )
+}
+
+export default Banner
